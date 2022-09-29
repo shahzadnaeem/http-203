c1be243fdd68a0151cec1cd728c783045e46340b
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-09-29 17:13:58 +0100

    Oh my!

--- Tailwind/src/main.js
+++ Tailwind/src/main.js
@@ -1,7 +1,11 @@
 // Let's do it all here!
 
+const uid = [0];
+
 class Builder {
-  constructor(mainEl, debug = false) {
+  static DEBUG = { ON: true, OFF: false };
+
+  constructor(mainEl, debug = DEBUG.OFF) {
     this.debug = debug;
     this.mainEl = mainEl;
     this.elCounts = {};
@@ -11,16 +15,19 @@ class Builder {
     // TODO: Assuming that elements that are cloned, are not added too!
     this.cloneSources = new Map();
     this.clones = 0;
+    this.badNestedClones = {};
   }
 
   stats() {
     return JSON.stringify(
       {
+        lastUID: uid[0],
         totalElems: this.elements - this.cloneSources.size + this.clones,
         numElems: this.elements - this.cloneSources.size,
         numClones: this.clones,
         counts: this.elCounts,
         clones: this.elClones,
+        badNestedClones: this.badNestedClones,
       },
       null,
       2
@@ -64,40 +71,106 @@ class Builder {
     return 0;
   }
 
+  incBadNestedCloneCount(name, numChildrenMissed) {
+    if (!this.badNestedClones[name]) {
+      this.badNestedClones[name] = { count: 0, childrenMissed: 0 };
+    }
+
+    this.badNestedClones[name].count++;
+    this.badNestedClones[name].childrenMissed += numChildrenMissed;
+  }
+
   add(el, to = this.mainEl) {
     if (typeof el === "function") {
       to.appendChild(el());
     } else {
       if (el instanceof Node) {
         to.appendChild(el);
+      } else if (Array.isArray(el) && el.every((el) => el instanceof Node)) {
+        to.append(...el);
       } else {
         throw new Error("Element is not a 'Node'!");
       }
     }
 
-    return this;
+    return to;
+  }
+
+  setUID(el) {
+    uid[0]++;
+
+    el.newElUID = `E.${uid[0]}/${el.nodeName}`;
+  }
+
+  clone(el) {
+    const cloned = el.cloneNode(true);
+    // TODO: How to really clone nested elements!
+    if (el.children.length) {
+      this.incBadNestedCloneCount(el.nodeName, el.children.length);
+
+      // TODO: This does not quite work :(
+
+      // while (cloned.firstChild) {
+      //   cloned.removeChild(cloned.firstChild);
+      // }
+
+      // for (const child of el.children) {
+      //   console.log(`Really deep cloning: ${child.newElUID}...`);
+      //   cloned.appendChild(this.clone(child));
+      // }
+    }
+    this.setUID(cloned);
+
+    return cloned;
   }
 
   newElement(name, classList = "", children = []) {
-    this.incElementCount(name);
-
     if (name) {
+      this.incElementCount(name);
+
       const element = document.createElement(name);
+
+      this.setUID(element);
       element.classList = classList;
 
       if (Array.isArray(children)) {
-        children.forEach((el) => {
+        children.forEach((el, i) => {
           if (typeof el === "function") {
             element.appendChild(el());
           } else {
+            let inception = el.children.length;
+
             // Clone elements and track them - stats track clones
             this.incCloneCount(el.nodeName, el);
-            const cloned = el.cloneNode(true);
+            const cloned = this.clone(el);
+
+            // if (inception) {
+            //   // Doh! Need to go deeper!
+            //   console.log(
+            //     `INCEPTION ALERT!: created ${name}/${cloned.newElUID} from ${name}/${element.newElUID} that has children!`
+            //   );
+            // }
+
             if (cloned.textContent) {
               if (this.debug) {
                 cloned.textContent += `🐑${this.getCloneCount(el.nodeName)}`;
+
+                if (inception) {
+                  let nodes = [];
+                  for (const child of el.children) {
+                    nodes.push(child.nodeName);
+                  }
+
+                  cloned.textContent =
+                    `🚩 ${cloned.newElUID} has ${
+                      el.children.length
+                    } child nodes [${nodes.join(
+                      ", "
+                    )}] FLATTENED as 'text' 🚩 ` + cloned.textContent;
+                }
               }
             }
+
             element.appendChild(cloned);
           }
         });
@@ -105,7 +178,9 @@ class Builder {
         if (typeof children === "string") {
           element.textContent = children;
         } else if (typeof children === "function") {
-          element.textContent = children(this.getElementCount(name));
+          element.textContent = children(
+            `${element.newElUID}-${this.getElementCount(name)}`
+          );
         } else {
           throw new Error("Invalid element 'content' (via children)");
         }
@@ -143,9 +218,15 @@ class Builder {
       .fill(itFn)
       .forEach((f) => f());
   }
+
+  makeNOf(n, itFn) {
+    return Array(n)
+      .fill(itFn)
+      .map((f) => f());
+  }
 }
 
-const init = (debug = false) => {
+const init = (debug = Builder.DEBUG.OFF) => {
   const mainEl = document.querySelector("#main");
 
   const builder = new Builder(mainEl, debug);
@@ -189,6 +270,66 @@ const init = (debug = false) => {
       builder.add(sectionWithParas, section);
     }
 
+    // Recreate above with the following code
+
+    const paras2 = builder.makeNOf(2, (n) =>
+      builder.newParagraph(
+        (n) => `Single makeNOf paragraph - #${n}`,
+        `w-full px-2 bg-teal-700 border-2 border-black`
+      )
+    );
+
+    builder.doNOf(2, () => {
+      const sectionWithParas = builder.newSection(paras2);
+      builder.add(sectionWithParas, section);
+    });
+
+    // This nesting of paragraphs in paragraphs fails!
+
+    const paras3 = builder.makeNOf(5, (n) =>
+      builder.newParagraph(
+        paras2,
+        `w-full px-2 bg-red-100 border-2 border-black text-black`
+      )
+    );
+
+    // NOTE: This step does work!
+
+    builder.add(paras3, section);
+
+    // TODO: This fails - related to cloning as creating <p> tags nested to 3 levels works
+    builder.add(builder.newSection(paras3), section);
+
+    // TODO: As does this - remove for now!
+
+    // builder.doNOf(2, () => {
+    //   const sectionWithParas = builder.newSection(paras3);
+    //   builder.add(sectionWithParas, section);
+    // });
+
+    // This alternative nesting of paragraphs in paragraphs works - builder.add() updated
+
+    builder.doNOf(2, () => {
+      builder.add(
+        builder.add(
+          builder.add(
+            builder.makeNOf(5, () =>
+              builder.newParagraph(
+                (n) => `${n} of 5 paras`,
+                `w-full px-2 bg-yellow-200 border-2 border-black text-black`
+              )
+            ),
+            builder.newParagraph(
+              (n) => `Mid Containing paragraph ${n}`,
+              `w-full px-2 bg-gray-200 border-2 border-white text-black`
+            )
+          ),
+          builder.newParagraph((n) => `Top Containing paragraph ${n}`)
+        ),
+        section
+      );
+    });
+
     builder.add(section);
   });
 
@@ -224,7 +365,9 @@ const init = (debug = false) => {
     )
   );
 
-  console.log(`builder_stats: ${builder.stats()}`);
+  if (debug) {
+    console.log(`builder_stats: ${builder.stats()}`);
+  }
 };
 
-init(true);
+init(Builder.DEBUG.ON);
