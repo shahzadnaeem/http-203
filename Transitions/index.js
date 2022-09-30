b830cc358c84902620a186e095033c5365375f4f
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-09-30 15:45:20 +0100

    You control the Matrix (simulator)!

--- Transitions/index.js
+++ Transitions/index.js
@@ -1,4 +1,5 @@
 const mainEl = document.querySelector("#main");
+const bothDirectionsCheckEl = document.querySelector("#bothDirections");
 
 const FIRSTRANDCHAR = 33;
 const LASTRANDCHAR = 96;
@@ -16,8 +17,82 @@ let chars = [];
 let charElems = [];
 let activeElems = [];
 
+// ----------------------------------------------------------------------------
+
+// TODO: Make this a frame based animation and manually adjust each cell per frame
+
+function fadeElement(el) {}
+
+function addFadingLine() {
+  const vertical = bothDirectionsMatrix ? Math.random() > 0.5 : true;
+  const start = Math.floor(Math.random() * SIZE);
+  const length = SIZE - start; // Math.floor(Math.random() * (SIZE - start));
+
+  if (length !== 0) {
+    const offset = Math.floor(Math.random() * SIZE);
+    const delta = vertical ? SIZE : 1;
+    const startIdx = vertical ? SIZE * start + offset : SIZE * offset + start;
+    const startDelay = 10;
+    const delayDelta = 50;
+
+    // Loop from the start to the end - vertical or horizontal
+    for (let i = start; i < start + length; i++) {
+      const idx = startIdx + (i - start) * delta;
+
+      const el = charElems[idx];
+
+      setTimeout(() => {
+        el.textContent = randomChar();
+        el.classList.add("fade");
+
+        activeElems[idx] = setTimeout(() => {
+          activeElems[idx] = false;
+          el.classList.remove("fade");
+          el.textContent = randomChar();
+        }, 300);
+      }, startDelay + (i - start) * delayDelta);
+    }
+  } else {
+    console.log(`addFadingLine: length === 0!`);
+  }
+}
+
+function randomCharChange() {
+  for (let i = 0; i < 10; i++) {
+    const idx = Math.floor(Math.random() * charElems.length);
+    const el = charElems[idx];
+    el.textContent = randomChar();
+  }
+}
+
+// ----------------------------------------------------------------------------
+
+let addFadingLineInterval = false;
+let randomCharChangeInterval = false;
+let bothDirectionsMatrix = true;
+
 function init() {
   console.log(`mainEl = ${mainEl.clientWidth} x ${mainEl.clientHeight}`);
+
+  mainEl.innerHTML = "";
+  if (addFadingLineInterval) {
+    clearInterval(addFadingLineInterval);
+    addFadingLineInterval = false;
+  }
+  if (randomCharChangeInterval) {
+    clearInterval(randomCharChangeInterval);
+    randomCharChangeInterval = false;
+  }
+
+  bothDirectionsCheckEl.checked = bothDirectionsMatrix;
+
+  function bothDirectionsListener(ev) {
+    bothDirectionsMatrix = ev.target.checked;
+    bothDirectionsCheckEl.removeEventListener("change", bothDirectionsListener);
+    init();
+  }
+
+  bothDirectionsCheckEl.addEventListener("change", bothDirectionsListener);
 
   const CALC_XSZ = Math.floor(mainEl.clientWidth / ITEMSZ);
   const XSZ = CALC_XSZ; // SIZE;
@@ -48,70 +123,10 @@ function init() {
 
   activeElems = chars.map((c) => false);
 
-  setInterval(addFadingLine, 60);
-  setInterval(randomCharChange, 10);
+  addFadingLineInterval = setInterval(addFadingLine, 30);
+  randomCharChangeInterval = setInterval(randomCharChange, 10);
 }
 
-function fadeElement(el) {}
-
-function addFadingLine() {
-  const vertical = Math.random() > 0.5;
-  const start = Math.floor(Math.random() * SIZE);
-  const length = SIZE - start; // Math.floor(Math.random() * (SIZE - start));
-
-  if (length !== 0) {
-    const offset = Math.floor(Math.random() * SIZE);
-    const delta = vertical ? SIZE : 1;
-    const startIdx = vertical ? SIZE * start + offset : SIZE * offset + start;
-    const startDelay = 10;
-    const delayDelta = 30;
-
-    // Loop from the start to the end - vertical or horizontal
-    for (let i = start; i < start + length; i++) {
-      const idx = startIdx + (i - start) * delta;
-
-      if (idx >= charElems.length) {
-        // How can this be???
-        console.log(
-          `DANGER: ${
-            vertical ? "V" : "H"
-          }: startIdx=${startIdx}, i=${i}, start=${start}, length=${length}, delta=${delta}`
-        );
-      }
-
-      const el = charElems[idx];
-
-      if (el) {
-        setTimeout(() => {
-          el.textContent = randomChar();
-          el.classList.add("fade");
-
-          activeElems[idx] = setTimeout(() => {
-            activeElems[idx] = false;
-            el.classList.remove("fade");
-            el.textContent = randomChar();
-          }, 300);
-        }, startDelay + (i - start) * delayDelta);
-      } else {
-        console.log(`addFadingLine: el undefined!`);
-        console.log(
-          `  ${
-            vertical ? "V" : "H"
-          }: start=${start}, length=${length} offset=${offset}, delta=${delta}, idx=${idx}`
-        );
-      }
-    }
-  } else {
-    console.log(`addFadingLine: length === 0!`);
-  }
-}
-
-function randomCharChange() {
-  for (let i = 0; i < 10; i++) {
-    const idx = Math.floor(Math.random() * charElems.length);
-    const el = charElems[idx];
-    el.textContent = randomChar();
-  }
-}
+// ----------------------------------------------------------------------------
 
 init();
