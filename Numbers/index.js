3fca136578e5c63d83d7a073f93a50b883432bfa
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-10-04 12:00:08 +0100

    Options and auto fit

--- Numbers/index.js
+++ Numbers/index.js
@@ -1,23 +1,85 @@
 const mainEl = document.querySelector("#main");
 const toEl = document.querySelector("#to");
+const resetEl = document.querySelector("#reset");
+const lettersEl = document.querySelector("#letters");
+const numLimitEl = document.querySelector("#num-limit");
 
 const START_NUM = 1;
 const END_NUM = 20;
 
+let letters = false;
+let numLimit = 0;
+
+function resetListener() {
+  init();
+}
+
+const resizeListener = (function makeResizeListener() {
+  const resizeDelay = 500;
+  let timeout = false;
+
+  return function resizeListener() {
+    clearTimeout(timeout);
+    timeout = setTimeout(init, resizeDelay);
+  };
+})();
+
+function lettersListener(ev) {
+  letters = ev.target.checked;
+  lettersEl.removeEventListener("change", lettersListener);
+
+  init();
+}
+
+function numLimitListener(ev) {
+  const newLimit = ev.target.value;
+
+  if (newLimit === "No limit") {
+    numLimit = 0;
+  } else {
+    numLimit = Number(newLimit);
+  }
+  numLimitEl.removeEventListener("input", numLimitListener);
+
+  init();
+}
+
 // ----------------------------------------------------------------------------
 
-function init() {
+function initControls() {
+  resetEl.removeEventListener("click", resetListener);
+  resetEl.addEventListener("click", resetListener);
+
+  lettersEl.checked = letters;
+  lettersEl.addEventListener("change", lettersListener);
+
+  numLimitEl.value = numLimit === 0 ? "No limit" : numLimit;
+  numLimitEl.addEventListener("input", numLimitListener);
+
+  removeEventListener("resize", resizeListener);
+  addEventListener("resize", resizeListener);
+}
+
+function initDisplay() {
   console.log(`mainEl = ${mainEl.clientWidth} x ${mainEl.clientHeight}`);
 
   const maxX = mainEl.clientWidth;
   const maxY = mainEl.clientHeight;
 
-  let CALC_END_NUM = Math.floor( maxX * maxY / ( 200 * 200 ) );
+  let CALC_END_NUM = Math.floor((maxX * maxY) / (200 * 200));
   CALC_END_NUM -= CALC_END_NUM % 5;
 
-  console.log(`CALC_END_NUM = ${CALC_END_NUM}`);
+  if ( CALC_END_NUM > 100 ) {
+    CALC_END_NUM = 100;
+  }
 
-  const SIZES = [75, 105, 145, 190, 225];
+  if (numLimit) {
+    CALC_END_NUM = Math.min(numLimit, CALC_END_NUM);
+  }
+
+  // console.log(`CALC_END_NUM = ${CALC_END_NUM}`);
+
+  const SIZES = [80, 110, 145, 190, 225, 250];
 
   const COLOURS = [
     "crimson",
@@ -49,12 +111,46 @@ function init() {
     return r1.l > r2.r || r1.r < r2.l || r1.t > r2.b || r1.b < r2.t;
   }
 
-  function isSafe(trial) {
+  function isSafe(rect) {
     if (extents.length === 0) return true;
 
-    return extents.every((ex) => {
-      return noOverlap(trial, ex);
+    return extents.every((ext) => {
+      return noOverlap(rect, ext);
     });
+  }
+
+  function newRect(x, y, sz) {
+    return {
+      l: x,
+      r: x + sz,
+      t: y,
+      b: y + sz,
+      sz: sz,
+    };
+  }
+
+  function findSafe(rect) {
+    const STARTX = Math.floor(Math.random() * 10);
+    const STARTY = Math.floor(Math.random() * 10);
+    const STEP = Math.floor(Math.random() * 5 + 5);
+
+    for (let x = STARTX; x < maxX - rect.sz; x += STEP) {
+      for (let y = STARTY; y < maxY - rect.sz; y += STEP) {
+        const alt = newRect(x, y, rect.sz);
+
+        if (isSafe(alt)) {
+          console.log(
+            `Found spot for ${JSON.stringify(rect)} now ${JSON.stringify(alt)}`
+          );
+
+          return alt;
+        }
+      }
+    }
+
+    console.log(`No safe place found for ${JSON.stringify(rect)}`);
+
+    return rect;
   }
 
   function getNumElem(num) {
@@ -67,23 +163,43 @@ function init() {
     let tryLimit = 50;
 
     do {
+      tryLimit --;
+
       x = Math.floor(Math.random() * (maxX - size));
       y = Math.floor(Math.random() * (maxY - size));
 
-      rect = { l: x, r: x + size, t: y, b: y + size, sz: size };
+      rect = newRect(x, y, size);
 
       safe = isSafe(rect);
 
       if (!safe) {
         if (tryLimit % 10 === 0) {
-          size = nextSmallerFromArray(SIZES, size);
+          const origRect = rect;
+          rect = findSafe(rect);
+          if (rect === origRect) {
+            size = nextSmallerFromArray(SIZES, size);
+          } else {
+            // Use 'new' rect coordinates
+            x = rect.l;
+            y = rect.t;
+
+            safe = true;
+          }
         }
       }
 
-      tryLimit--;
-      if (tryLimit === 0) {
-        console.log(`out of luck!: ${JSON.stringify(rect)} for ${num}`);
-        overlaps ++;
+      if (!safe && tryLimit === 0) {
+        const origRect = rect;
+        rect = findSafe(rect);
+        if (rect === origRect) {
+          overlaps++;
+        }
+
+        // Use 'new' rect coordinates
+        x = rect.l;
+        y = rect.t;
+
+        // We give up, but may have a new location
         safe = true;
       }
     } while (!safe);
@@ -92,20 +208,23 @@ function init() {
 
     const div = document.createElement("div");
     div.textContent = num;
+    div.classList.add("number");
 
+    // Custom styles
     div.style.setProperty("left", `${x}px`);
     div.style.setProperty("top", `${y}px`);
     div.style.setProperty("height", `${size}px`);
     div.style.setProperty("width", `${size}px`);
 
-    div.style.setProperty("font-size", `${size / 20.0}rem`);
+    div.style.setProperty("font-size", `${size / 22.0}rem`);
     div.style.setProperty("color", `${col}`);
 
-    const borderWidth = Math.max(5, Math.floor(size / 25));
+    const borderWidth = 5; //Math.max(5, Math.floor(size / 25));
     div.style.setProperty(
       "border",
       `${borderWidth}px solid ${randomFromArray(COLOURS.slice(2))}`
     );
+    div.style.setProperty("border-radius", "25%");
 
     extents.push(rect);
 
@@ -114,31 +233,34 @@ function init() {
 
   mainEl.innerHTML = "";
 
-  for (let num = START_NUM; num <= CALC_END_NUM; num++) {
-    mainEl.appendChild(getNumElem(num));
+  if (letters) {
+    for (let l = 1; l <= 26; l++) {
+      mainEl.appendChild(getNumElem(String.fromCharCode(l + 64)));
+    }
+  } else {
+    for (let num = START_NUM; num <= CALC_END_NUM; num++) {
+      mainEl.appendChild(getNumElem(num));
+    }
   }
 
-  if ( overlaps ) {
-    overlaps = `(${overlaps} overlaps!)`;
+  if (overlaps) {
+    overlaps = `(${overlaps} overlap${overlaps > 1 ? "s" : ""}!)`;
   } else {
     overlaps = "";
   }
 
-  toEl.textContent = `${START_NUM} to ${CALC_END_NUM} ${overlaps}`;
-
-  removeEventListener("resize", resizeListener);
-  addEventListener("resize", resizeListener);
+  if (letters) {
+    toEl.textContent = `A to Z ${overlaps}`;
+  } else {
+    toEl.textContent = `${START_NUM} to ${CALC_END_NUM} ${overlaps}`;
+  }
 }
 
-const resizeListener = (function makeResizeListener() {
-  const resizeDelay = 500;
-  let timeout = false;
+function init() {
+  initControls();
 
-  return function resizeListener() {
-    clearTimeout(timeout);
-    timeout = setTimeout(init, resizeDelay);
-  };
-})();
+  initDisplay();
+}
 
 // ----------------------------------------------------------------------------
 
