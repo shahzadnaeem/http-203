6a7c16d1eb5dab717ade261a1dd409d71b01112f
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-10-05 17:12:04 +0100

    Add simple count

--- Template/index.js
+++ Template/index.js
@@ -6,6 +6,7 @@ const resetEl = document.querySelector("#reset");
 const check1El = document.querySelector("#check1");
 const clockEl = document.querySelector("#clock");
 
+let count = 0;
 let check1 = true;
 
 function resetListener() {
@@ -21,22 +22,24 @@ function initControls() {
   resetEl.removeEventListener("click", resetListener);
   resetEl.addEventListener("click", resetListener);
 
-  trackCheckbox( check1El, check1, check1Listener );
+  trackCheckbox(check1El, check1, check1Listener);
 }
 
 function initDisplay() {
+  count++;
+
   mainEl.innerHTML = "";
-  subHeadingEl.textContent = "Hello, I'm ready...";
+  subHeadingEl.textContent = `Hello, I'm ready... #${count}`;
 
-  const div = document.createElement('div');
-  div.className = 'box';
-  div.textContent = `${JSON.stringify(getViewportInfo(), 0 , 2)}`;
+  const div = document.createElement("div");
+  div.className = "box";
+  div.textContent = `#${count}\n${JSON.stringify(getViewportInfo(), 0, 2)}`;
 
-  if ( check1 ) {
-    div.classList.add('red');
+  if (check1) {
+    div.classList.add("red");
   }
   mainEl.appendChild(div);
-  
+
   clock(clockEl);
 }
 
