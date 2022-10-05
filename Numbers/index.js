7507a8a9b8c4035a37c956e181d7055750164dbf
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-10-05 19:02:58 +0100

    2nd message on height hack

--- Numbers/index.js
+++ Numbers/index.js
@@ -77,9 +77,8 @@ function initDisplay() {
   if ( maxY === 0 ) {
     // This is wrong :(
     maxY = document.documentElement.clientHeight - 106;  // SAD FACE
+    console.log(`!! mainEl = ${maxX} x ${maxY}`);
   }
-
-  console.log(`mainEl = ${maxX} x ${maxY}`);
 
   let CALC_END_NUM = Math.floor((maxX * maxY) / (200 * 200));
   CALC_END_NUM -= CALC_END_NUM % 5;
